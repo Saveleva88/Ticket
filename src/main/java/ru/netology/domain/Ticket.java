350f51fb59611e8d703c3f060a25d12040657975
Irina Saveleva <fomina_88@mail.ru>
2021-11-23 20:44:25 +0400

    Added files

--- src/main/java/ru/netology/domain/Ticket.java
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -1,4 +1,22 @@
 package ru.netology.domain;
 
-public class Ticket {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Ticket implements Comparable<Ticket> {
+    private int id;
+    private int price;
+    private String departureAirport;
+    private String arrivalAirport;
+    private int time;
+
+    @Override
+    public int compareTo(Ticket o) {
+        return this.price - o.price;
+    }
 }
